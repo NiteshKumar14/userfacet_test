5096e369ddb4c3b2ca51bfb54eb89d7da8b31875
Niteshkumar14 <niteshkr@yahoo.in>
2022-07-29 13:27:54 +0530

    added notify

--- backend/index.js
+++ backend/index.js
@@ -13,13 +13,10 @@ const availableSlots = require('./teacher_availability.json');
 app.post('/api/scheduleClass', async (req, res) => {
     try {
         const { weekday, start_time, end_time } = req.body;
-        console.log(availableSlots.availability);
         const week = availableSlots.availability;
 
         if (weekday.toLowerCase() in week) {
             const day = week[weekday.toLowerCase()];
-            console.log("day", day);
-
             const slot = day.filter((time) => time.start_time === start_time && time.end_time === end_time)
             console.log("lots", slot);
             if (slot[0]) {
